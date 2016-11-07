741999b66609a8c86530e88d2356429508ef31b3
BourMel <meline.bour-lang@etu.unistra.fr>
2016-11-07 21:47:29 +0100

    Video.js
    Intégration du frameworks video.js et modification du lecteur.
    Initialisation du volet de droite.

--- js/main.js
+++ js/main.js
@@ -1,45 +1,27 @@
+//questions à poser :
+//vidéo vraiment plein écran
+//#lecteurSource is not a function
+//possible de récupérer lieu, année, durée sans data-attribute ?
+
 $(document).ready(function() {
    console.log('jQuery ready');
     
     //charge la première vidéo de la liste
     
     var lecteur = $('#lecteur');
+    var source = $("#lecteurSource")
     var liste = $('#listeVideos div');
     var first = liste.first();
+    var burger = $('#burger');
+    var volet = $('section');
     
     var firstLien = first.attr('data-lien');
     var firstPoster = first.attr('data-poster');
-    lecteur.attr('src', firstLien);
+    lecteurSource.attr('src', firstLien);
     lecteur.attr('poster', firstPoster);
-    
-    //bouton play/pause
-    
-    var play = $('#play');
-    var isplaying = false;
-    
-    play.text('>');
-    play.css('display', 'block');
-    
-    function read () {
-        if (isplaying == false) {
-            lecteur[0].play();
-            isplaying = true;
-            play.css('display', 'none');
-        } else {
-            lecteur[0].pause();
-            isplaying = false;
-            play.text('>');
-            play.css('display', 'block');
-        }
-    }
-    
-    lecteur.click(read);
-    play.click(read);
     
     //menu hamburger
     
-    var burger = $('#burger');
-    var volet = $('section');
     var isOpen = false;
     burger.click(function() {
         if(isOpen === false) {
@@ -52,14 +34,28 @@ $(document).ready(function() {
         
     });
     
-    //clic dans la liste = vidéo lue
+    //clic dans la liste = vidéo lue + fermeture burger
     
     liste.click(function () {
+        //récupération de la vidéo
         var lien = $(this).attr('data-lien');
         var poster = $(this).attr('data-poster');
         console.log(lien);
         console.log(poster);
-        lecteur.attr('src', lien);
+        
+        //récupération des attributs
+        var premierePhrase = $(this).attr('data-premierePhrase');
+        var description = $(this).attr('data-description');
+        //var personne = $(this).children('ul').children('.personne').html();
+        //lieu
+        //année
+        //durée
+        console.log(premierePhrase);
+        console.log(description);
+        
+        lecteurSource.attr('src', lien);
         lecteur.attr('poster', poster);
+        //fermeture burger
+        volet.hide();
     });
 });
